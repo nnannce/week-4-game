00a227f42d96e8e9f3e9a3e1fc190e898b4a6a04
Nancy M <nancym@Nancys-Air.PK5001Z>
2017-11-25 10:03:59 -0600

    more js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,20 @@
 
 var randomResult;
-var lost;
-var won;
+var loss = 0;
+var won = 0;
 var previous= 0;
 
+
+var resetStart = function() {
+
+$(".crystals").empty();
+
+var images = [
+	'assets/images/amethyst.jpg',
+	'assets/images/diamond.jpg',
+	'assets/images/sapphire.jpg',
+	'assets/images/topaz.jpg',
+]
 
 randomResult = Math.floor(Math.random() * 99) + 20; 
 
@@ -12,7 +23,7 @@ $("#result").html('Random Result: ' + randomResult);
 for(var i=0; i<4; i++){
 
 	var random = Math.floor(Math.random()*11) + 1;
-	console.log(random);
+	
 
 	var crystal = $("<div>");
 		crystal.attr({
@@ -20,25 +31,45 @@ for(var i=0; i<4; i++){
 			"data-random": random
 		});
 
-		crystal.html(random);
+	crystal.css({
+		"background-image":"url('" + images[i]+ "')",
+		"background-size":"cover"
+	});
+
+		
 
 	$(".crystals").append(crystal);
 		
 }
 
+$("#previous").html("Total Score:" + previous);}
 
-$(".crystal").on('click', function() {
+resetStart();
+
+
+
+$(document).on('click', ".crystal", function() {
 
 	var num = parseInt($(this).attr('data-random'));
 	previous += num;
 
+$("#previous").html("Total Score:" + previous);
+
 	console.log(previous);
 
 	if(previous > randomResult){
-		console.log("You lost!");
+		loss++;
+		$("#loss").html("Losses:  " + loss);
+
+		previous = 0;
+
+		resetStart();
 
 	}
 	else if(previous === randomResult){
-	console.log("You win!");
+		won++;
+		$("#win").html("Wins:  " + won);
+		previous = 0;
+		resetStart();
 	}
 });
